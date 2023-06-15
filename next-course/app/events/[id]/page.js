530a2a136b40a65c7f692306aa7e3951b6225774
seumbae <ohcm0013@gmail.com>
2023-06-15 19:19:11 +0900

    update: add Event Detail components

--- next-course/app/events/[id]/page.js
+++ next-course/app/events/[id]/page.js
@@ -1,7 +1,32 @@
+"use client";
+import { useParams } from "next/navigation";
+
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { getEventById } from "@/lib/dummy-data";
+
 export default function EventDetail() {
+	const param = useParams();
+
+	const event = getEventById(param.id);
+
+	if (!event) {
+		return <p>No event Found!</p>;
+	}
+
 	return (
-		<div>
-			<h1>The Event Detail Page</h1>
-		</div>
+		<>
+			<EventSummary title={event.title} />
+			<EventLogistics
+				date={event.date}
+				address={event.location}
+				image={event.image}
+				imageAlt={event.title}
+			/>
+			<EventContent>
+				<p>{event.description}</p>
+			</EventContent>
+		</>
 	);
 }
