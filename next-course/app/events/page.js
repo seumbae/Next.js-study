123e4aa48317170ce482889afb88a3400069c101
seumbae <ohcm0013@gmail.com>
2023-06-15 21:18:47 +0900

    update: add fineEventsHandler using router

--- next-course/app/events/page.js
+++ next-course/app/events/page.js
@@ -1,15 +1,21 @@
 'use client';
+import { useRouter } from "next/navigation";
 
 import { getAllEvents } from "@/lib/dummy-data";
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
 
 export default function AllEvents() {
+	const router = useRouter();
 	const events = getAllEvents();
 
+	function findEventsHandler(year, month) {
+		const fullPath = `/events/${year}/${month}`
+		router.push(fullPath);
+	}
 	return (
 		<>
-			<EventsSearch />
+			<EventsSearch onSearch={findEventsHandler}/>
 			<EventList items={events} />
 		</>
 	);
