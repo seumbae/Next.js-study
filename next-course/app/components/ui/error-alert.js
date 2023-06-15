bfc951d15a07779e05f555945e58ae30da3ec8d1
seumbae <ohcm0013@gmail.com>
2023-06-15 21:42:42 +0900

    create: Error component

---
+++ next-course/app/components/ui/error-alert.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export default function ErrorAlert(props) {
+	return <Alert>{props.children}</Alert>;
+}
+
+const Alert = styled.div`
+	margin: 1rem auto;
+	padding: 1rem 2rem;
+	width: 90%;
+	max-width: 40rem;
+	background-color: #d5bdfc;
+	color: #38028d;
+	font-weight: bold;
+	font-size: 1.5rem;
+	text-align: center;
+	border-radius: 6px;
+`;
