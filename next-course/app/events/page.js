4f7211b993a30ea3944cd9520e7fcbe99def9300
seumbae <ohcm0013@gmail.com>
2023-06-15 21:07:16 +0900

    update: add Seacrh Component

--- next-course/app/events/page.js
+++ next-course/app/events/page.js
@@ -2,13 +2,15 @@
 
 import { getAllEvents } from "@/lib/dummy-data";
 import EventList from "@/components/events/event-list";
+import EventsSearch from "@/components/events/events-search";
 
 export default function AllEvents() {
 	const events = getAllEvents();
 
 	return (
-		<div>
+		<>
+			<EventsSearch />
 			<EventList items={events} />
-		</div>
+		</>
 	);
 }
