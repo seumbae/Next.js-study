d17d4477ebaa6fb28d8169e3ab07ff97088ca8e9
seumbae <ohcm0013@gmail.com>
2023-06-15 21:43:02 +0900

    update: filtered event page

--- next-course/app/events/[...slug]/page.js
+++ next-course/app/events/[...slug]/page.js
@@ -1,7 +1,73 @@
+"use client";
+import { useParams } from "next/navigation";
+import styled from "styled-components";
+
+import { getFilteredEvents } from "@/lib/dummy-data";
+import EventList from "@/components/events/event-list";
+import ResultsTitle from "@/components/events/results-title";
+import Button from "@/components/ui/button";
+import ErrorAlert from "@/components/ui/error-alert";
+
 export default function FilteredEvents() {
+	const params = useParams();
+
+	const filtered = params.slug.split("/");
+	if (!filtered) {
+		return <Paragraph>Loading...</Paragraph>;
+	}
+
+	const filteredYear = +filtered[0];
+	const filteredMonth = +filtered[1];
+
+	if (
+		isNaN(filteredYear) ||
+		isNaN(filteredMonth) ||
+		filteredYear > 2030 ||
+		filteredYear < 2021 ||
+		filteredMonth < 1 ||
+		filteredMonth > 12
+	) {
+		return (
+			<>
+				<ErrorAlert>
+					<p>Invalid Filter. Please adjust your values!</p>;
+				</ErrorAlert>
+				<Frg>
+					<Button link="/events">Show All Events</Button>
+				</Frg>
+			</>
+		);
+	}
+
+	const filteredEvents = getFilteredEvents({
+		year: filteredYear,
+		month: filteredMonth,
+	});
+
+	if (!filteredEvents || filteredEvents.length === 0) {
+		return (
+			<>
+				<ErrorAlert>
+					<p>No events found for the chosen filter!</p>
+				</ErrorAlert>
+				<Frg>
+					<Button link="/events">Show All Events</Button>
+				</Frg>
+			</>
+		);
+	}
+
+	const date = new Date(filteredYear, filteredMonth - 1);
+
 	return (
-		<div>
-			<h1>Filtered Events Page</h1>
-		</div>
+		<>
+			<ResultsTitle date={date} />
+			<EventList items={filteredEvents} />
+		</>
 	);
 }
+
+const Frg = styled.div`
+	margin: auto;
+	text-align: center;
+`;
